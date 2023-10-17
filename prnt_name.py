a9d38aa87b176efc52bb7b7273592ed9da328b2a
pfb15 <pfb15@info15.cshl.edu>
2023-10-17 11:03:47 -0400

    added f"" string formatting

--- prnt_name.py
+++ prnt_name.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-print("My name: David")
+name = "David"
+
+print(f"My name: {name}")
 print("My favority color: Green")
 
 fav_activity = "Reading books"
 
-print("My favority leisure activity:" , fav_activity)
+print(f"My favority leisure activity: {fav_activity}")
+
+print("My favority animal: Me, probably")
 
