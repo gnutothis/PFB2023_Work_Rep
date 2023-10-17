65770b0c4b60c029deb55205a5f87597a403ef64
pfb15 <pfb15@info15.cshl.edu>
2023-10-17 15:04:46 -0400

    pirates, man

---
+++ py_prob_2/pirate_math.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+fav_letter = sys.argv[1]
+
+if fav_letter == 'c':
+  print('Yarr, me first love!')
+
+elif fav_letter == 'r':
+  print('Ye pick \'em well, but me first love is the \'c\'!')
+
+else:
+  print('Ye be walkin\' the plank!')
